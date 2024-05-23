233b0ec3f965f405090341493552328058f84f2f
mimoteken <152700935+mimoteken@users.noreply.github.com>
2024-05-23 03:36:46 +0200

    Update Homme.java

--- Homme.java
+++ Homme.java
@@ -10,8 +10,8 @@ public class Homme extends Citoyen {
      * Default constructor
      */
     
-        public Homme(int num_id, String nom, String prenom, int age) {
-            super(num_id, nom, prenom, age);
+        public Homme(int num_id, String nom, String prenom, int age,Mairie mairieNaissance) {
+            super(num_id, nom, prenom, age, mairieNaissance);
             // Initialisez les autres attributs spécifiques à homme si nécessaire
             this.liste_naissance = new HashSet<>();
             this.liste_mariage = new HashSet<>();
